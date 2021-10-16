add5b5ba9874a975c5382cf9da19f4f3d36685f4
pdromoreno7 <pdro.moreno96@gmail.com>
2021-10-16 15:42:49 -0500

    Se añadio home a app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
-import { Hero } from "./components/Hero/Hero";
+
 import Navbar from "./components/Navbar/Navbar";
+import { Home } from "./containers/Home";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Hero />
+
+      <Home />
     </>
   );
 }
